9edcd067892d7ce9253d3577b8867230355c1de8
Zarinamdk <reizar2004@mail.ru>
2022-12-06 09:11:56 +0300

    Converter update

---
+++ PhysicValuesLib/Values/Speed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysicValuesLib.Values
+{
+    internal class Speed : AnyValue
+    {
+        public Speed()
+        {
+            ValueName = "Скорость";
+            CoefficientsAndMeasuresList = new Dictionary<string, double>()
+            {
+                {"Метр в секунду", 1},
+                {"Метр в минуту", 0.01666666667},
+                {"Километр в секунду",1000},
+                {"Километр в час", 0.27777777778},
+            };
+        }
+    }
+}
